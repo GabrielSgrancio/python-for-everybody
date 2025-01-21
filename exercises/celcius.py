063bf643785a69f4d59406c6d5084e77a93dc88a
GabrielSgrancio <biel.sgrancio@hotmail.com>
2025-01-21 16:59:28 -0300

    ch1-exercises

---
+++ exercises/celcius.py
@@ -0,0 +1,5 @@
+celcius_temp = input("Enter temp (in celcius): ")
+
+farenheit = (float(celcius_temp) * 1.8) + 32
+
+print("The temperature in F is:", farenheit)
---
+++ exercises/hour-rate.py
@@ -0,0 +1,6 @@
+hours = float(input("Enter hours:"))
+rate = float(input("Enter Rate:"))
+
+pay = (hours*rate)
+print(f"Pay: {pay:.2f}")
+print(round(pay,3))
